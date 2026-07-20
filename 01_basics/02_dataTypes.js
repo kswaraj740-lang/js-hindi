0f0bd2eefc81683dd47456281e85d46b4a79a9e1
kswaraj740-lang <kswaraj740@gmail.com>
2026-07-20 16:34:58 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" ; // it treats all js code as newer version
+
+//alert( 3 + 3) // we are using nodejs , not browser 
+
+console.log("swaraj")
+
+
+let name = "swaraj"
+let age = 18 
+let isLoggedIn = true
+let state;
+
+
+// number => 2 to power 53
+//bigint 
+//string => " "
+//boolean => true /false
+// null => standalone value 
+//symbol => unique 
+// undefined => 
+
+
+//object 
+
+console.log(typeof undefined) //undefined 
+console.log(typeof null ) // object 
